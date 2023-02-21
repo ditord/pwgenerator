8d1eb11d448aa474921eb9e8689f8925a4c20774
Artur Papyan <angry.root@gmail.com>
2023-02-22 01:19:55 +0400

    Minor improvements, separated script from html

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
-function generatePassword() {
-  // prompt user for password length
-  var length = parseInt(prompt("How long would you like your password to be? (Must be between 8 and 128 characters)"));
+      function generatePassword() {
+        // get password length
+        var length = parseInt(document.getElementById("password-length").value);
+      
+        // get character type selections
+        var includeUppercase = document.getElementById("include-uppercase").checked;
+        var includeLowercase = document.getElementById("include-lowercase").checked;
+        var includeDigits = document.getElementById("include-digits").checked;
+        var includeSymbols = document.getElementById("include-symbols").checked;
 
-  // validate password length
-  if (isNaN(length) || length < 8 || length > 128) {
-    alert("Invalid password length. Please enter a number between 8 and 128.");
-    return;
-  }
+        // validate password length
+        if (isNaN(length) || length < 8 || length > 128) {
+          alert("Invalid password length. Please enter a number between 8 and 128.");
+          return;
+        }
 
-  // prompt user for character types
-  var includeUppercase = confirm("Would you like to include uppercase letters?");
-  var includeLowercase = confirm("Would you like to include lowercase letters?");
-  var includeDigits = confirm("Would you like to include digits?");
-  var includeSymbols = confirm("Would you like to include symbols?");
+        // validate at least one character type is selected
+        if (!includeUppercase && !includeLowercase && !includeDigits && !includeSymbols) {
+          alert("Please select at least one character type.");
+          return;
+        }
 
-  // validate at least one character type is selected
-  if (!includeUppercase && !includeLowercase && !includeDigits && !includeSymbols) {
-    alert("Please select at least one character type.");
-    return;
-  }
+        // define character sets
+        var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+        var digitChars = "0123456789";
+        var symbolChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
 
-  // define character sets
-  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var digitChars = "0123456789";
-  var symbolChars = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+        // build character pool based on user selections
+        var characterPool = "";
+        if (includeUppercase) {
+          characterPool += uppercaseChars;
+        }
+        if (includeLowercase) {
+          characterPool += lowercaseChars;
+        }
+        if (includeDigits) {
+          characterPool += digitChars;
+        }
+        if (includeSymbols) {
+          characterPool += symbolChars;
+        }
 
-  // build character pool based on user selections
-  var characterPool = "";
-  if (includeUppercase) {
-    characterPool += uppercaseChars;
-  }
-  if (includeLowercase) {
-    characterPool += lowercaseChars;
-  }
-  if (includeDigits) {
-    characterPool += digitChars;
-  }
-  if (includeSymbols) {
-    characterPool += symbolChars;
-  }
+        // generate password
+        var password = "";
+        for (var i = 0; i < length; i++) {
+          var randomIndex = Math.floor(Math.random() * characterPool.length);
+          password += characterPool.charAt(randomIndex);
+        }
 
-  // generate password
-  var password = "";
-  for (var i = 0; i < length; i++) {
-    var randomIndex = Math.floor(Math.random() * characterPool.length);
-    password += characterPool.charAt(randomIndex);
-  }
-
-  return password;
-}
+        return password;
+      }
+      
+      var form = document.getElementById("password-form");
+      form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        var passwordDisplay = document.getElementById("password-display");
+        var password = generatePassword();
+        passwordDisplay.innerText = password;
+      });
